18d0efae31a0eb3f0380c0d939033c613b2285c3
Imal-Kovinda <83854345+Imal-Kovinda@users.noreply.github.com>
2023-07-14 23:08:57 +0530

    day1

---
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employees-list',
+  templateUrl: './employees-list.component.html',
+  styleUrls: ['./employees-list.component.css']
+})
+export class EmployeesListComponent {
+  // employees: Employee[] = [
+  //   {
+  //     id: '1245',
+  //     name: 'Jsmin',
+  //     salary: 120000,
+  //     phone: 714682499,
+  //   },
+  //   {
+  //     id: '1246',
+  //     name: 'Kovinda',
+  //     salary: 160000,
+  //     phone: 714682489,
+  //   }
+  // ];
+  constructor() {}
+
+  ngOnInit(): void{
+    
+  }
+
+}
